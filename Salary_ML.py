eab21df0160e5b87f34951546f7bd04a92a82e98
Stephane Tekam <tsephane@yahoo.fr>
2021-01-30 17:42:47 +0100

    refactor find_optimal_lm_mode

--- Salary_ML.py
+++ Salary_ML.py
@@ -37,7 +37,7 @@ def clean_data(df):
 
     # Dummy the categorical variables
     cat_vars = df.select_dtypes(include=['object']).copy().columns
-    for var in  cat_vars:
+    for var in cat_vars:
         # for each cat add dummy var, drop original column
         df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)], axis=1)
 
